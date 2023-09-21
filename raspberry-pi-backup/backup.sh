372fa2b342e5e7af3daa22e0d4da751ebbab442f
Manuel <mr-manuel@outlook.it>
2023-09-21 10:22:59 +0200

    added error handling

--- raspberry-pi-backup/backup.sh
+++ raspberry-pi-backup/backup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# script version 0.0.2 (2023.06.07)
+# script version 0.0.3 (2023.09.21)
 
 # uncomment for debugging
 #set -x
@@ -22,6 +22,7 @@ BACKUP_REMOTE_MOUNT_USER="username"
 BACKUP_REMOTE_MOUNT_PW="password"
 
 # how may backups should be kept?
+# Use 0 to disable old backup deletion
 BACKUP_COUNT="5"
 
 # backup hostname (default gets the hostname from the system)
@@ -35,12 +36,26 @@ BACKUP_MOUNT="/mnt/backup"
 BACKUP_PATH="$BACKUP_MOUNT/$BACKUP_SUBFOLDER$BACKUP_HOSTNAME"
 BACKUP_NAME="Backup_$BACKUP_HOSTNAME"
 
+echo
+
 # create mount dir if not exists
 if [ ! -d $BACKUP_MOUNT ]; then
+    echo "$BACKUP_MOUNT does not exist. Creating folder..."
     mkdir $BACKUP_MOUNT
+    echo
+fi
+
+# check if something is already mounted
+if [ 1 -eq "$(mount -v | grep -c $BACKUP_MOUNT)" ]; then
+    echo "WARNING: There is already mounted something to \"$BACKUP_MOUNT\". This will be unmounted now."
+    echo
+    mount -v | grep $BACKUP_MOUNT
+    umount $BACKUP_MOUNT
+    echo
 fi
 
 # mount harddisk
+echo "Mounting \"$BACKUP_REMOTE_MOUNT\" to \"$BACKUP_MOUNT\"..."
 mount -t cifs -o user=$BACKUP_REMOTE_MOUNT_USER,password=$BACKUP_REMOTE_MOUNT_PW,rw,file_mode=0777,dir_mode=0777 $BACKUP_REMOTE_MOUNT $BACKUP_MOUNT
 
 if [ $? -ne 0 ]; then
@@ -48,30 +63,52 @@ if [ $? -ne 0 ]; then
     exit
 fi
 
+echo
+
 # create folder if it does not exist
 if [ ! -d "$BACKUP_PATH" ]; then
+    echo "Creating \"$BACKUP_SUBFOLDER\" on backup mount..."
     mkdir -p "$BACKUP_PATH"
     if [ $? -ne 0 ]; then
         echo "Error when creating of the backup folder on the remote path."
+        echo
         exit
     fi
+    echo
 fi
 
 # check if dd is a symlink like in busybox
-if [[ -L "/bin/dd" ]] || [[ -f "/opt/victronenergy/version" ]]
-# if not use the system dd
-then
-    # create backup
-    $SCRIPT_DIR/ext/dd if=/dev/mmcblk0 of=${BACKUP_PATH}/${BACKUP_NAME}_$(date +%Y%m%d_%H%M%S).img bs=1MB status=progress
-
 # if yes then probably the argument "status=progress" will not work, use own dd
-else
+if [[ -L "/bin/dd" ]] || [[ -f "/opt/victronenergy/version" ]]; then
+    # check if backup is already running
+    if [ "$(ps | grep -c 'dd if=/dev/mmcblk0')" -gt 1 ]; then
+        echo "Backup already running. Exiting..."
+        echo
+        exit
+    fi
     # create backup
-    dd if=/dev/mmcblk0 of=${BACKUP_PATH}/${BACKUP_NAME}_$(date +%Y%m%d_%H%M%S).img bs=1MB status=progress
+    echo "Using script dd for backup."
+    "$SCRIPT_DIR/ext/dd" if=/dev/mmcblk0 of="${BACKUP_PATH}/${BACKUP_NAME}_$(date +%Y%m%d_%H%M%S).img" bs=1MB status=progress
+
+# if not use the system dd
+else
+    # check if backup is already running
+    if [ "$(ps -aux | grep -c 'dd if=/dev/mmcblk0')" -gt 1 ]; then
+        echo "Backup already running. Exiting..."
+        echo
+        exit
+    fi
+    # create backup
+    echo "Using system dd for backup."
+    /bin/dd if=/dev/mmcblk0 of="${BACKUP_PATH}/${BACKUP_NAME}_$(date +%Y%m%d_%H%M%S).img" bs=1MB status=progress
 fi
 
-if [ $? -ne 0 ]; then
+if [ $? -eq 0 ]; then
+    echo "Backup completed successfully."
+    echo
+else
     echo "Error when backup of the system."
+    echo
     exit
 fi
 
@@ -79,9 +116,18 @@ fi
 # delete old backups
 BACKUP_FILES_TO_DELETE_COUNT=$(ls -tr ${BACKUP_PATH}/${BACKUP_NAME}* | head -n -${BACKUP_COUNT} | wc -l)
 if [ "$BACKUP_FILES_TO_DELETE_COUNT" -ne "0" ]; then
-    pushd ${BACKUP_PATH}; ls -tr ${BACKUP_PATH}/${BACKUP_NAME}* | head -n -${BACKUP_COUNT} | xargs rm; popd
+    pushd ${BACKUP_PATH} || exit
+    ls -tr ${BACKUP_PATH}/${BACKUP_NAME}* | head -n ${BACKUP_COUNT} | xargs rm
+    popd || exit
     echo -e "$BACKUP_FILES_TO_DELETE_COUNT old backups deleted."
 fi
 
 # unmount harddisk
 umount $BACKUP_MOUNT
+
+if [ $? -ne 0 ]; then
+    echo "Error when unmounting the remote path."
+fi
+
+echo
+echo
